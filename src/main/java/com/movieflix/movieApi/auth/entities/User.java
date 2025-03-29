673b87492f19c304d25f067d30c9298518ba0d57
HungNguyen0210 <hung.nlm03@gmail.com>
2025-03-29 12:08:40 +0700

    User entities

---
+++ src/main/java/com/movieflix/movieApi/auth/entities/User.java
@@ -0,0 +1,89 @@
+package com.movieflix.movieApi.auth.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    @NotBlank(message = "The name field can't be blank")
+    private String name;
+
+    @NotBlank(message = "The username field can't be blank")
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank(message = "The email field can't be blank")
+    @Column(unique = true)
+    @Email(message = "Please enter email in proper format")
+    private String email;
+
+    @NotBlank(message = "The password field can't be blank")
+    @Size(min = 5, message = "The password must have at least 5 character")
+    private String password;
+
+    private RefreshToken refreshToken;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    private boolean isEnabled = true;
+
+    private boolean isAccountNonExpired = true;
+
+    private boolean isAccountNonLocked = true;
+
+    public boolean isCredentialsNonExpired = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
